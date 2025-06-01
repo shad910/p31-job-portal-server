1fa4b62ff998fa79992c4a97a6ca57edbf57da1a
shad910 <shadshs91@gmail.com>
2025-06-01 12:02:56 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const port = 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json);
+
+app.listen(port,()=>{console.log(`Server is running on this ${port} port.`)})
